0151ca7f8bb3f379c05273bba737fa44885f1755
Ekomjah Denis <ekomjahedet@gmail.com>
2025-09-02 19:25:16 +0100

    Initialize the tic-tac-toe app repo

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const xoArr = new Array(9).fill(null);
+  const [isClicked, setIsClicked] = useState(true);
+  const [msg, setMsg] = useState("X starts!");
+  const [gameArr, setGameArr] = useState(xoArr);
+  function reset() {
+    setGameArr(xoArr);
+    setIsClicked(true);
+    setMsg("X starts!");
+  }
+  function gameShow(index) {
+    setGameArr((prev) => {
+      let newArr = prev.map((label, gameIndex) => {
+        if (msg.includes("winner") || label !== null) {
+          return label;
+        }
+        if (label === null && gameIndex === index) {
+          setIsClicked(!isClicked);
+          setMsg(isClicked ? "Next Player: O" : "Next Player: X");
+          return isClicked ? "X" : "O";
+        }
+        return label;
+      });
+
+      const winArrIndex = gameLogic(newArr);
+      if (winArrIndex !== undefined) {
+        setIsClicked(isClicked);
+        setMsg(isClicked ? "X is the winner!" : "O is the winner!");
+      } else if (newArr.every((val) => val !== null)) {
+        setIsClicked(isClicked);
+        setMsg("Its a draw!");
+      }
+
+      return newArr;
+    });
+  }
+
+  function gameLogic(arr) {
+    const winArr = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winArr.find(
+      (group, index) =>
+        group.every((item, i) => arr[item] === "X") ||
+        group.every((item, i) => arr[item] === "O")
+    );
+  }
+
+  return (
+    <>
+      <h1>A Tic-Tac-Toe game!</h1>
+      <div className="btn-container">
+        {msg && <p className="message">{msg}</p>}
+
+        {gameArr.map((item, index) => (
+          <button
+            className="square"
+            key={index}
+            onClick={() => {
+              gameShow(index);
+            }}
+          >
+            {item}
+          </button>
+        ))}
+        <button onClick={reset} className="reset" id="reset">
+          RESET BOARD
+        </button>
+      </div>
+      <a href="https://www.exploratorium.edu/explore/puzzles/tictactoe">
+        How to play?
+      </a>
+    </>
+  );
+}
+
+export default App;
